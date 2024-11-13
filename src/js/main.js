6305c70adc1373b10cec6b68f5b5b52c67d5b907
OkuroYume <yuma.comazzi@studentfr.ch>
2024-11-13 17:09:04 +0100

    idk what im doing but i look hot doing it

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("DOMContentLoaded", function () {
   gsap.to(".slider-container", {
-    x: "-80%", // chaque élément .slider-item vaut 20% de .slider-container, on veut donc bouger la position x de -80% pour faire apparaitre le dernier élément: chaque élément a besoin de 20% pour apparaitre pleinement sur l'écran.
+    x: "-80%",
     scrollTrigger: {
       trigger: ".slider-mask",
       start: "top top",
@@ -14,3 +14,71 @@ window.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".parallax-container",
+      start: "top top",
+      scrub: 1,
+      pin: true,
+    },
+  })
+  .to("#element-3", {
+    y: "-5%",
+    x: "5%",
+  })
+  .to(
+    "#element-1",
+    {
+      y: "-5",
+    },
+    0
+  )
+  .to(
+    "#element-2",
+    {
+      y: "-5%",
+    },
+    0
+  )
+  .to(
+    "#element-4",
+    {
+      y: "5%",
+    },
+    0
+  );
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".wrapper",
+      start: "top top",
+      scrub: true,
+      pin: true,
+    },
+  })
+  .to("#bulle-1", {
+    left: "27%",
+    top: "13%",
+    opacity: 1,
+  })
+  .to("#bulle-2", {
+    left: "12%",
+    top: "78%",
+    opacity: 1,
+  })
+  .to("#bulle-3", {
+    left: "67%",
+    top: "55%",
+    opacity: 1,
+  })
+  .to("#bulle-4", {
+    left: "53%",
+    top: "25%",
+    opacity: 1,
+  })
+  .to("#bulle-5", {
+    left: "60%",
+    top: "95%",
+    opacity: 1,
+  });
